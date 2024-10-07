768517695148a0a225aaec2986364d62939691ca
Delbert <delbertdrums5@gmail.com>
2024-10-07 19:43:03 +0100

    Done with day 24

--- day24_weather_app/lib/home_page.dart
+++ day24_weather_app/lib/home_page.dart
@@ -10,39 +10,79 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xff104084),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: ListView(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const Icon(
-                  Icons.location_on_outlined,
-                  color: Colors.white,
-                ),
-                const Text(
-                  'Fortaleza',
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xff29B2DD),
+            Color(0xff33AADD),
+            Color(0xff2DC8EA),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: ListView(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(
+                        Icons.location_on_outlined,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        'Fortaleza',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Icon(
+                        Icons.keyboard_arrow_down_sharp,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  Image.asset('assets/i.png'),
+                ],
+              ),
+              Image.asset(
+                'assets/sun.png',
+                height: 170,
+              ),
+              const Center(
+                child: Text(
+                  '30º',
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 60,
                   ),
                 ),
-                const Icon(
-                  Icons.keyboard_arrow_down_sharp,
-                  color: Colors.white,
-                ),
-                Expanded(
-                  child: Image.asset('assets/i.png'),
-                ),
-              ],
-            ),
-          ],
+              ),
+              Center(
+                child: Image.asset('assets/o.png'),
+              ),
+              Image.asset('assets/1.png'),
+              Image.asset('assets/2.png'),
+              Image.asset('assets/2.png'),
+            ],
+          ),
         ),
       ),
     );
